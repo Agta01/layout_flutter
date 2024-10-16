bcade0657dd21ddd32bbe2271a3e855504bc577d
Agta Fadjrin Aminullah <fadjrinagta@gmail.com>
2024-10-17 04:15:03 +0700

    Praktikum 2 JS 6

--- lib/main.dart
+++ lib/main.dart
@@ -42,10 +42,20 @@ class MyApp extends StatelessWidget {
       ],
     ),
   );
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+  Widget buttonSection = Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      );
     return MaterialApp(
       title: 'Flutter layout: Agta Fadjrin Aminullah dan 2241760072',
       home: Scaffold(
@@ -55,12 +65,33 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
             ],
-            ),  // Perbaikan: gunakan titleSection di body
+            ),  
       ),
     );
   }
 }
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
 
 class MyHomePage extends StatefulWidget {
